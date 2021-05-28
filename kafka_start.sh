0eecd76e2faec18b650d25e5064bdf227a06f0bf
Anton Ohorodnyk <anton.ohorodnyk@aura.com>
2021-05-28 16:28:13 -0700

    Add kafka host

--- kafka_start.sh
+++ kafka_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+KAFKA_HOST="${KAFKA_HOST:-kafka}"
 KAFKA_ADVERTISED_LISTENERS="${KAFKA_ADVERTISED_LISTENERS:-PLAINTEXT://kafka:9092}"
 sed -iserver.properties.back "s#advertised.listeners=PLAINTEXT://localhost:9092#advertised.listeners=${KAFKA_ADVERTISED_LISTENERS}#g" /opt/kafka/config/kraft/server.properties
+
+echo "$(/sbin/ip route | egrep -o '[0-9\.]+' | tail -n1)   ${KAFKA_HOST}" >> /etc/hosts
 
 kafka-server-start.sh $@
